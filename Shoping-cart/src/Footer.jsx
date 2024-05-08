0752bb9ef17484f725d3c383500ba14e12c1c479
BalavigneshRajasekar <you@example.com>
2024-05-08 13:19:07 +0530

    Footer defined

---
+++ Shoping-cart/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-dark text-center py-5">
+      <p className="text-light">Copyright © Your Website 2023</p>
+    </div>
+  );
+}
+
+export default Footer;
