93d1e58b34bfa0c757c1be2add2dd5beef63b01e
BalavigneshRajasekar <you@example.com>
2024-05-08 13:19:18 +0530

    Banner defined

--- Shoping-cart/src/Banner.jsx
+++ Shoping-cart/src/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Banner() {
   return (
-    <div className="bg-dark text-center text-light banner">
+    <div className="bg-dark text-center text-light banner   mt-2">
       <h1 className="banner-h1">Shop in Style</h1>
       <p className="banner-p">With this shop homepage template</p>
     </div>
